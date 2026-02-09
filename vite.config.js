8fafed4b8279b3dd60cca389d4af0db6c56c811b
msiyem <siyem2574@gmail.com>
2026-02-09 12:46:39 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: import.meta.env.VITE_BASE_PATH || '/pos_system/',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base: env.VITE_BASE_PATH || '/pos_system/',
+  }
 })
