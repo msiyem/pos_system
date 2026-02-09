e32d7fc4bc74245cd464f6a1a0461ee29a41dc7c
msiyem <siyem2574@gmail.com>
2026-02-09 12:29:03 +0600

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: import.meta.env.VITE_VITE_PATH || '/pos_system/',
 })
